d92cb8bc385fe4844cdba03e9bf528c8d53101ad
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-11-25 15:58:59 +0000

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const mesImages = document.getElementById("mes-images");
 const voirPlus = document.getElementById("seeMore");
-console.log(voirPlus);
 
 voirPlus.addEventListener("click", () => {
-  let nImg = 6;
-  for (let n = 0; n < nImg; n++) {
-    let imageUrl = `https://picsum.photos/300?random=${Math.floor(Math.random() * 1000)}`;
+  let nombreImg = 6;
+  for (let n = 0; n < nombreImg; n++) {
+    let imageUrl = `https://picsum.photos/300?random=${Math.floor(
+      Math.random() * 500
+    )}`;
     let img = document.createElement("img");
+    console.log(imageUrl);
     img.src = imageUrl;
     mesImages.appendChild(img).innerHTML = `${imageUrl}`;
   }
